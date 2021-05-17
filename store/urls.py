655a4fc2d8ad1821fd0a434befb11a13e947e898
Grayson-choi <784wldnd@gmail.com>
2021-05-18 00:02:51 +0900

    category 별로 확인하기

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.store, name='store'),
+   path('', views.store, name='store'), # 메인 화면 보여줌
+   path('<slug:category_slug>/', views.store, name='products_by_category'),
 ] 
