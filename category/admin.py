dd61edccc595cf34b3f54161b3a1b8b80fbf7b0f
Grayson-choi <784wldnd@gmail.com>
2021-05-25 17:45:47 +0900

    slug 자동 입력 코드 완성

--- category/admin.py
+++ category/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Category
 
 # Register your models here.
-admin.site.register(Category)
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)} # slug 자동 입력
+    list_display = ('category_name', 'slug')
+
+admin.site.register(Category, CategoryAdmin)
