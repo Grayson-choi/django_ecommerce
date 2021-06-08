dfec1a28b650d0244212e390c9892cc59b264119
Grayson-choi <784wldnd@gmail.com>
2021-06-08 20:22:24 +0900

    cart 페이지 add cart 구현

--- carts/views.py
+++ carts/views.py
@@ -36,8 +36,8 @@ def add_cart(request, product_id):
         )
         cart_item.save()
     
-    return HttpResponse(cart_item.quantity)
-    exit()
+    # return HttpResponse(cart_item.quantity)
+    # exit()
     return redirect('cart')
 
 
@@ -48,16 +48,16 @@ def add_cart(request, product_id):
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_actice=True)
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {
-        'total', total,
-        'quantity', quantity,
-        'cart_items', cart_items
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
     }
-    return render(request, 'store/cart.html')
+    return render(request, 'store/cart.html', context)
